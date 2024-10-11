000589aa929795edab143f06b4c227885e6e093e
Peter Cosemans <peter.cosemans@gmail.com>
2024-10-11 17:45:14 +0200

    fix: invalid tailwind styling

--- packages/frontend-ui-react/src/utils/cn.tsx
+++ packages/frontend-ui-react/src/utils/cn.tsx
@@ -1,7 +1,8 @@
-import { clsx, type ClassValue } from 'clsx';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   // we are wrapping this to make it easier to mock in tests
   // and allow some extendability in the future
-  return clsx(...inputs);
+  return twMerge(clsx(...inputs));
 }
