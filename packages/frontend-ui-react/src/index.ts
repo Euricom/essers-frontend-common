424cc05201632d022e11f29df013d057dfd020c5
Stijn Dams <stijn.dams@essers.com>
2024-10-11 13:19:36 +0200

    fix: export dialog provider

--- packages/frontend-ui-react/src/index.ts
+++ packages/frontend-ui-react/src/index.ts
@@ -3,6 +3,7 @@ export * from './components/button';
 export * from './components/pagination';
 export * from './components/localeProvider';
 export * from './components/dialog';
+export * from './components/dialogProvider';
 
 // hooks
 
